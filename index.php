a70697736ebb8eb4973827593094ee523ad59229
Catherine <cathey191@hotmail.com>
2018-12-07 09:50:55 +1300

    Added title to index

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
   get_header();
 ?>
       <div class="container main">
+        <h2 class="text-center pageTitle"><?php the_title(); ?></h2>
       <?php
         if(have_posts()):
         while(have_posts()): the_post();
