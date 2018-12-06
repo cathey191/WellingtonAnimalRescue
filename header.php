acc382d21d61fb6eb49fc6e00c2220c45f3d1f6d
Catherine <cathey191@hotmail.com>
2018-12-06 13:48:10 +1300

    Removed Woocommerce breadcrumbs

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
   </head>
-  <body>
+  <body <?php body_class(); ?>>
     <nav class="navbar navbar-expand-lg navbar-dark " role="navigation">
       <div class="container">
         <a class="navbar-brand" href="<?= bloginfo('home');?>">
--- index.php
+++ index.php
@@ -7,19 +7,7 @@
         while(have_posts()): the_post();
       ?>
 
-        <div class="row">
-          <?php if( has_post_thumbnail()): ?>
-            <div class="d-none d-sm-block col-6 ">
-              <?php the_post_thumbnail('medium_large', ['class'=>'card-img-top img-fluid', 'alt'=> 'Image of animal']) ?>
-            </div>
-          <?php endif; ?>
-          <div class="col-sm-6 home-text">
-            <h2><?= the_title(); ?></h2>
-            <?php if (get_the_content()): ?>
-              <p><?= the_content(); ?></p>
-            <?php endif; ?>
-          </div>
-        </div>
+        <?= the_content(); ?>
 
       <?php
         endwhile;
